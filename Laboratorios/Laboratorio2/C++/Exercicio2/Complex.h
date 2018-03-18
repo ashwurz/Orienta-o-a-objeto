6d82c013974a64fc6464beb0c62b47867a825730
ashwurz <37230151+ashwurz@users.noreply.github.com>
2018-03-18 20:08:43 -0300

    Update Complex.h

--- Laboratorios/Laboratorio2/C++/Exercicio2/Complex.h
+++ Laboratorios/Laboratorio2/C++/Exercicio2/Complex.h
@@ -1,10 +1,19 @@
+/*
+ -> Helmuth August Risch Filho 
+ -> RA: 22.117.038-4
+ ->
+ -> CC3642 - Orientação a Objetos
+ ->
+ -> O programa faz a leitura do nome, sobrenome e salario de duas pessoas e imprime todas as leituras, utilizando as funções get e set
+ */
 #include <string>
 
 using namespace std;
-
+//Criação da classe Complex
 class Complex
 {
 	public:
+		//Criação do construtor
 		Complex(double real, double imaginario);
 		double complexSoma1(double a, double b);
 		double complexSoma2(double c, double d);
