60b51bad20903e17436236dd06ee67d092075307
ashwurz <37230151+ashwurz@users.noreply.github.com>
2018-03-18 20:55:53 -0300

    Update main.cpp

--- Laboratorios/Laboratorio2/C++/Exercicio2/main.cpp
+++ Laboratorios/Laboratorio2/C++/Exercicio2/main.cpp
@@ -1,9 +1,18 @@
+/*
+ -> Helmuth August Risch Filho 
+ -> RA: 22.117.038-4
+ ->
+ -> CC3642 - Orientação a Objetos
+ ->
+ -> O programa faz a leitura do nome, sobrenome e salario de duas pessoas e imprime todas as leituras, utilizando as funções get e set
+ */
 #include <iostream>
 #include "Complex.h"
 #include <string>
 
 int main(int argc, char** argv) {
 	using namespace std;
+	//Declaração das variáveis
 	double real;
 	double imaginario;
 	double numeroComplexo;
@@ -12,29 +21,30 @@ int main(int argc, char** argv) {
 	double subtraiReal;
 	double subtraiImaginario;
 	int escolha;
-	
+	//O usuário define o valor das variáveis
 	cout<<"Informe a parte real do numero complexo:"<<endl;
 	cin>>real;
 	cout<<"Informe a parte imaginaria do numero complexo"<<endl;
 	cin>>imaginario;
-	
+	//Criação do objeto complexo1
 	Complex complexo1 = Complex(real,imaginario);
-	
+	//O usuário define o valor das variáveis
 	cout<<"Informe a parte real do segundo numero complexo:"<<endl;
 	cin>>real;
 	cout<<"Informe a parte imaginaria do segundo numero complexo:"<<endl;
 	cin>>imaginario;
-	
+	//Criação do objeto complexo2
 	Complex complexo2 = Complex(real,imaginario);
+	//O usuário define qual função será chamada
 	cout<<"Digite 1 para soma de numeros complexos ou 2 para subtracao de numeros complexos:"<<endl;
 	cin>>escolha;
-	
+	//Chama a função de soma dos números imaginários
 	if(escolha == 1){
 		somaReal = complexo1.complexSoma1(complexo1.getReal(), complexo2.getReal());
 		somaImaginario = complexo1.complexSoma2(complexo1.getImaginario(),complexo2.getImaginario());
 		complexo1.complexImprime(somaReal, somaImaginario);
 	}
-	
+	//Chama função de subtração dos números imaginários
 	if(escolha == 2){
 		subtraiReal = complexo1.complexSubtrai1(complexo1.getReal(),complexo2.getReal());
 		subtraiImaginario = complexo1.complexSubtrai2(complexo1.getImaginario(),complexo2.getImaginario());
