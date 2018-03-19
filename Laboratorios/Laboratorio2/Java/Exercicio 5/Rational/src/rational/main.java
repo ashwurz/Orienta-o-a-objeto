b6fb361d01755a84ea92da53935a8a29ce67186d
ashwurz <37230151+ashwurz@users.noreply.github.com>
2018-03-18 21:31:26 -0300

    Update main.java

--- Laboratorios/Laboratorio2/Java/Exercicio 5/Rational/src/rational/main.java
+++ Laboratorios/Laboratorio2/Java/Exercicio 5/Rational/src/rational/main.java
@@ -1,29 +1,37 @@
-
+/*
+ -> Helmuth August Risch Filho 
+ -> RA: 22.117.038-4
+ ->
+ -> CC3642 - Orientação a Objetos
+ ->
+ */
 package rational;
 import java.util.Scanner;
 public class main {
     public static void main(String args[]){
+        //Declaração das variáveis
         int numerador;
         int denominador;
         int escolha = 0;
+        //Criação do objeto Resposta
         Rational resp = new Rational();
         Scanner input = new Scanner( System.in );
-        
+        //Usuário define o valor das variáveis
         System.out.println("Digite o valor do numerador da primeira fracao:");
         numerador = input.nextInt();
         System.out.println("Digite o valor do denominador da primeria fracao:");
         denominador = input.nextInt();
-        
+        //Criação do objeto fração1
         Rational fracao1 = new Rational(numerador,denominador);
-        
+        //Usuário define novamente as variáveis
         System.out.println("Digite o valor do numerador da segunda fracao:");
         numerador = input.nextInt();
         System.out.println("Digite o valor do denominador da segunda fracao:");
         denominador = input.nextInt();
-        
+        //Criação do objeto fração2
         Rational fracao2 = new Rational(numerador,denominador);
         
-        
+        //Usuário escolhe entre as 5 operações básicas e de acordo com sua escolha, calcula e imprime a fração resultante simplificada
         while(escolha != 5){
             System.out.println("Digite 1 para Soma, 2 para Subtracao, 3 para Multiplicacao, 4 para Divisao ou 5  para sair do programa:");
             escolha = input.nextInt();
