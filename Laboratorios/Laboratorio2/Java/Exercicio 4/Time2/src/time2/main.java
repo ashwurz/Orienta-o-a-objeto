ef1920f12267ee729676717617a443678d416f9f
ashwurz <37230151+ashwurz@users.noreply.github.com>
2018-03-18 21:10:37 -0300

    Update main.java

--- Laboratorios/Laboratorio2/Java/Exercicio 4/Time2/src/time2/main.java
+++ Laboratorios/Laboratorio2/Java/Exercicio 4/Time2/src/time2/main.java
@@ -1,4 +1,11 @@
-
+/*
+ -> Helmuth August Risch Filho 
+ -> RA: 22.117.038-4
+ ->
+ -> CC3642 - Orientação a Objetos
+ ->
+ -> O programa faz a leitura do nome, sobrenome e salario de duas pessoas e imprime todas as leituras, utilizando as funções get e set
+ */
 package time2;
 
 
@@ -8,14 +15,17 @@ public class main
 
     public static void main(String[] args)
     {
+        //Criação do objeto
         Time2 horario = new Time2(22, 58, 59);
-
+        //Imprime o horario atual
         System.out.println(horario.toString());
-
+        //Chamando a função de incrementar hora
         horario.incrementHour();
+        //Chamadno a função de incrementar minuto
         horario.incrementMinute();
+        //Chamando a função de incrementar segundo
         horario.tick();
-
+        //Impressão do resultado em String
         System.out.println(horario.toString());
 
     }
